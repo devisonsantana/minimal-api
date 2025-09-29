756792c263f84e362d75ec3bbe9056c7fba0049a
devisonsantana <devisonsantana@hotmail.com>
2025-09-29 19:29:51 -0300

    feat: Added POST Vehicle endpoint
    New POST mapping to vehicle allows create a new vehicle and save on database

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using minimal_api.Domain.DTOs;
+using minimal_api.Domain.Entities;
 using minimal_api.Domain.Interfaces;
 using minimal_api.Domain.ModelViews;
 using minimal_api.Domain.Services;
@@ -12,9 +13,11 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        #region Builder
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddScoped<IAdministratorService, AdministratorService>();
+        builder.Services.AddScoped<IVehicleService, VehicleService>();
 
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
@@ -27,10 +30,14 @@ public class Program
             );
         });
         var app = builder.Build();
+        #endregion
 
+        #region Home
         app.MapGet("/", () => Results.Json(new Home()));
+        #endregion
 
-        app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministratorService administrator) =>
+        #region Administrator
+        app.MapPost("/administrator/login", ([FromBody] LoginDTO loginDTO, IAdministratorService administrator) =>
         {
             if (administrator.Login(loginDTO) != null)
             {
@@ -41,9 +48,26 @@ public class Program
                 return Results.Unauthorized();
             }
         });
+        #endregion
 
+        #region Vehicle endpoint
+        app.MapPost("/vehicle", ([FromBody] VehicleDTO vehicleDTO, IVehicleService vehicleService) =>
+        {
+            var vehicle = new Vehicle
+            {
+                Name = vehicleDTO.Name,
+                Brand = vehicleDTO.Brand,
+                Year = vehicleDTO.Year
+            };
+            vehicleService.Save(vehicle);
+            return Results.Created($"/vehicle/{vehicle.Id}", vehicle);
+        });
+        #endregion
+
+        #region Using Swagger and SwaggerIU
         app.UseSwagger();
         app.UseSwaggerUI();
+        #endregion
 
         app.Run();
     }
