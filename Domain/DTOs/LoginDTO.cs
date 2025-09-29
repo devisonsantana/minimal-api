15951ffeef2e96469493b0bf0b0f16ba24dfd0e0
devisonsantana <devisonsantana@hotmail.com>
2025-09-29 19:28:50 -0300

    feat: Vehicle DTO created
    Create a record to Vehicle DTO and change class to record on LoginDTO

--- Domain/DTOs/LoginDTO.cs
+++ Domain/DTOs/LoginDTO.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace minimal_api.Domain.DTOs
 {
-    public class LoginDTO
+    public record LoginDTO
     {
         public string Email { get; set; } = default;
         public string Password { get; set; } = default;
