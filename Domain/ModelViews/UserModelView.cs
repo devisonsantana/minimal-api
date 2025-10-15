e6e8df7233e95da5440a3139de7641179758a3d9
devisonsantana <devisonsantana@hotmail.com>
2025-10-15 00:00:44 -0300

    feat: add to UserModelView a constructor

--- Domain/ModelViews/UserModelView.cs
+++ Domain/ModelViews/UserModelView.cs
@@ -1,9 +1,18 @@
+using minimal_api.Domain.Entities;
+
 namespace minimal_api.Domain.ModelViews
 {
     public record UserModelView
     {
-        public int Id { get; set; } = default;
-        public string Email { get; set; } = default;
-        public string Role { get; set; } = default;
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public UserModelView() { }
+        public UserModelView(User user)
+        {
+            Id = user.Id;
+            Email = user.Email;
+            Role = user.Role;
+        }
     }
 }
